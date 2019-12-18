735dcf7c4d7631f16ed105123554a95049242d27
Hitendra Singh Shekhawat <sshitendra010@gmail.com>
2019-12-18 18:16:19 +0530

    Awesome BarCodeScanner
    in NewApp.js the old version of scanner is there from qr scanner plugin.
    
    in App.js new version is there which uses CameraKitCameraScreen plugin.

--- barcode/barcodescanner/App.js
+++ barcode/barcodescanner/App.js
@@ -1,69 +1,133 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-'use strict';
- 
+//This is an example code to Scan QR code//
 import React, { Component } from 'react';
- 
-import {
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  Linking,
-} from 'react-native';
- 
-import QRCodeScanner from 'react-native-qrcode-scanner';
-
-class ScanScreen extends Component {
-  onSuccess = (e) => {
-    Linking
-      .openURL(e.data)
-      .catch(err => console.error('An error occured', err));
+//import react in our code.
+import { Text, View, Linking, TouchableHighlight, PermissionsAndroid, Platform, StyleSheet} from 'react-native';
+// import all basic components
+import { CameraKitCameraScreen, } from 'react-native-camera-kit';
+//import CameraKitCameraScreen we are going to use.
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      //variable to hold the qr value
+      qrvalue: '',
+      opneScanner: false,
+    };
   }
- 
+  onOpenlink() {
+    //Function to open URL, If scanned 
+    Linking.openURL(this.state.qrvalue);
+    //Linking used to open the URL in any browser that you have installed
+  }
+  onBarcodeScan(qrvalue) {
+    //called after te successful scanning of QRCode/Barcode
+    this.setState({ qrvalue: qrvalue });
+    this.setState({ opneScanner: false });
+  }
+  onOpneScanner() {
+    var that =this;
+    //To Start Scanning
+    if(Platform.OS == 'android'){
+      async function requestCameraPermission() {
+        try {
+          const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.CAMERA,{
+              'title': 'CameraExample App Camera Permission',
+              'message': 'CameraExample App needs access to your camera '
+            }
+          )
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            //If CAMERA Permission is granted
+            that.setState({ qrvalue: '' });
+            that.setState({ opneScanner: true });
+          } else {
+            alert("CAMERA permission denied");
+          }
+        } catch (err) {
+          alert("Camera permission err",err);
+          console.warn(err);
+        }
+      }
+      //Calling the camera permission function
+      requestCameraPermission();
+    }else{
+      that.setState({ qrvalue: '' });
+      that.setState({ opneScanner: true });
+    }    
+  }
   render() {
+    let displayModal;
+    //If qrvalue is set then return this view
+    if (!this.state.opneScanner) {
+      return (
+        <View style={styles.container}>
+            {/*<Text style={styles.heading}>React Native QR Code Example</Text> */}
+            <Text style={styles.simpleText}>{this.state.qrvalue ? 'Scanned QR Code: '+this.state.qrvalue : ''}</Text>
+            {this.state.qrvalue.includes("http") ? 
+              <TouchableHighlight
+                onPress={() => this.onOpenlink()}
+                style={styles.button}>
+                  <Text style={{ color: '#FFFFFF', fontSize: 12 }}>Open Link</Text>
+              </TouchableHighlight>
+              : null
+            }
+            <TouchableHighlight
+              onPress={() => this.onOpneScanner()}
+              style={styles.button}>
+                <Text style={{ color: '#FFFFFF', fontSize: 12 }}>
+                Open QR Scanner
+                </Text>
+            </TouchableHighlight>
+        </View>
+      );
+    }
     return (
-      <QRCodeScanner
-        onRead={this.onSuccess}
-       // flashMode={QRCodeScanner.Constants.FlashMode.torch} 
-        topContent={
-          <Text style={styles.centerText}>
-            Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.
-          </Text>
-        }
-        bottomContent={
-          <TouchableOpacity style={styles.buttonTouchable}>
-            <Text style={styles.buttonText}>OK. Got it!</Text>
-          </TouchableOpacity>
-        }
-      />
+      <View style={{ flex: 1 }}>
+        <CameraKitCameraScreen
+          showFrame={false}
+          //Show/hide scan frame
+          scanBarcode={true}
+          //Can restrict for the QR Code only
+          laserColor={'blue'}
+          //Color can be of your choice
+          frameColor={'yellow'}
+          //If frame is visible then frame color
+          colorForScannerFrame={'black'}
+          //Scanner Frame color
+          onReadCode={event =>
+            this.onBarcodeScan(event.nativeEvent.codeStringValue)
+          }
+        />
+      </View>
     );
   }
 }
- 
 const styles = StyleSheet.create({
-  centerText: {
+  container: {
     flex: 1,
-    fontSize: 18,
-    padding: 32,
-    color: '#777',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor:'white'
   },
-  textBold: {
-    fontWeight: '500',
-    color: '#000',
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#2c3539',
+    padding: 10,
+    width:300,
+    marginTop:16
   },
-  buttonText: {
-    fontSize: 21,
-    color: 'rgb(0,122,255)',
+  heading: { 
+    color: 'black', 
+    fontSize: 24, 
+    alignSelf: 'center', 
+    padding: 10, 
+    marginTop: 30 
   },
-  buttonTouchable: {
-    padding: 16,
-  },
+  simpleText: { 
+    color: 'black', 
+    fontSize: 20, 
+    alignSelf: 'center', 
+    padding: 10, 
+    marginTop: 16
+  }
 });
- 
-export default ScanScreen;
