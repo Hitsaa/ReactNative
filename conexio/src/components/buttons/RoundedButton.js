d87fdb087a05ee89970ab2d59d96d5da52acba10
Hitendra Singh Shekhawat <sshitendra010@gmail.com>
2019-12-31 11:18:29 +0530

    conexio project started
    login screen is integrated.

---
+++ conexio/src/components/buttons/RoundedButton.js
@@ -0,0 +1,51 @@
+import React, {Component}from 'react';
+import {
+    Text,
+    View,
+    TouchableHighlight,
+    StyleSheet,
+}from 'react-native';
+import PropTypes from 'prop-types';
+import colors from '../../style/color';
+export default class RoundedButton extends Component{
+    render() {
+        const { text, icon,textColor, backgroundColor } = this.props;
+    //    const backgroundColor = background || 'transparent'
+    //    const color = textColor || color.black
+        return(
+            <TouchableHighlight 
+            onPress={() => alert('Button pressed')}
+            style={[{backgroundColor},styles.wrapper]}>                            
+                <Text style={[{textColor},styles.buttonText]}>
+                    {text}                    
+                </Text>                                    
+            </TouchableHighlight>                        
+        );
+    }
+}
+
+RoundedButton.PropTypes = {
+    text: PropTypes.string.isRequired,
+    textColor: PropTypes.colors,    //it is not required.
+    backgroundColor: PropTypes.string
+};
+
+const styles=StyleSheet.create({
+    wrapper:{
+        padding:15,
+        //flex:1,
+        flexDirection:'row',
+        display:"flex",
+        borderRadius:40,
+        borderWidth:1,
+        borderColor:colors.white,
+        marginBottom:15,
+        alignItems:'center',
+    },
+    buttonText:{
+        fontSize:16,
+        width:"100%",
+        textAlign:'center',
+        color:colors.green01
+    },    
+});
---
+++ conexio/src/screen/LoggedOut.js
@@ -0,0 +1,96 @@
+import React, {Component} from 'react';
+import {
+    Text,
+    View,
+    StyleSheet,
+    Image,
+    TouchableHighlight,
+    ScrollView,
+    ImageBackground,
+    Linking,
+}from 'react-native';
+import { SocialIcon } from 'react-native-elements';
+import colors from "../style/color";
+import RoundedButton from "../components/buttons/RoundedButton";
+export default class LoggedOut extends Component {
+onOpenlink() {
+    Linking.openURL("https://www.facebook.com");
+}
+    render() {
+        return (
+            <ImageBackground 
+            source={require("../images/football1.jpg")}
+            style={styles.BgImage}
+            imageStyle={{resizeMode: 'stretch'}}>                
+                <View style={styles.wrapper}>
+                    <View style={styles.welcomeWrapper}>
+                        <Image 
+                            style={styles.logo}
+                            source={require("../images/mylogo.png")}>                    
+                        </Image>                
+                        <Text
+                            style={styles.welcomeText}>
+                            connect with sports professional.
+                        </Text>
+                        <RoundedButton
+                            text="Sign up with Email"
+                            textColor={colors.green01}
+                            backgroundColor={colors.white}                            
+                        />
+                        <RoundedButton text="Sign up as Scout" textColor={colors.white} />
+                        <View style={[styles.iconstyle,{paddingBottom:15}]}>
+                        <SocialIcon
+                        type="facebook"                        
+                        onPress={() => {this.onOpenlink()}}
+                        />
+                        <SocialIcon
+                        type="google"
+                        onPress={() => {this.onOpenlink()}}
+                        />
+                        <SocialIcon
+                        type="instagram"
+                        onPress={() => {this.onOpenlink()}}
+                        />
+                        </View>
+                        <RoundedButton text="I already have an account" textColor={colors.white} />
+                    </View>
+                </View>
+            </ImageBackground>
+);
+}}
+
+const styles = StyleSheet.create({
+    wrapper:{
+        flex:1,
+        //backgroundColor: colors.green01,
+        //justifyContent:'center',
+        alignItems:'center',
+    },
+    welcomeWrapper:{
+        marginTop:30,
+        padding:20,
+        alignItems:'center',
+    },
+    logo:{
+        width:100,
+        height:50,
+        marginTop:50,
+        marginBottom:20,        
+    },
+    BgImage:{
+        width:null,
+        height:null,
+        flex:1,        
+    },
+    welcomeText:{
+        fontSize:18,
+        color:'yellow',
+        textAlign:'left',
+        marginBottom:200
+    },
+    iconstyle:{
+        //flex:1,
+        flexDirection:'row',        
+        marginBottom:100,
+    }
+})
